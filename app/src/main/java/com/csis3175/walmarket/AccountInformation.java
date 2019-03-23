3769edbf6c63e7efb83756aed0ae7abbcac80faf
300294055 <jaskirat3095@gmail.com>
2019-03-23 16:44:13 -0700

    AccountInformation

--- app/src/main/java/com/csis3175/walmarket/AccountInformation.java
+++ app/src/main/java/com/csis3175/walmarket/AccountInformation.java
@@ -8,21 +8,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.csis3175.walmarket.database.UserDbHelper;
+import com.csis3175.walmarket.entity.User;
+import com.csis3175.walmarket.util.SessionUtil;
 
 
 public class AccountInformation extends Fragment {
 
     UserDbHelper userDbHelper;
+  User user;
+  MainActivity mainActivity;
 
 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        user = SessionUtil.getUser(getActivity());
+        final EditText etfname= mainActivity.findViewById(R.id.etfName);
+        etfname.setText(user.getfName());
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account_information, container, false);
-
 
     }
 
